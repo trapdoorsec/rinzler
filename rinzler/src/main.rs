ab3fe9b4eeb112f69d0914201a26b274ab72409e
akses0 <akses0@pm.me>
2025-12-12 19:00:41 +1030

    fmt

--- rinzler/src/main.rs
+++ rinzler/src/main.rs
@@ -8,21 +8,23 @@ fn main() {
         .version(env!("CARGO_PKG_VERSION"))
         .bin_name("rinzler")
         .styles(CLAP_STYLING)
-        .arg(arg!(--"quiet")
-            .required(false))
+        .arg(arg!(--"quiet").required(false))
         .subcommand_required(true)
         .subcommand(
             command!("scan")
-                .arg(arg!(--"url" -u <URL>)
+                .arg(
+                    arg!(--"url" -u <URL>)
                         .required(false)
                         .help("The IP address to scan")
                         .value_parser(clap::value_parser!(Url))
-                        .default_value("http://127.0.0.1"))
-                .arg(arg!(--"hosts-file" -H <PATH>)
+                        .default_value("http://127.0.0.1"),
+                )
+                .arg(
+                    arg!(--"hosts-file" -H <PATH>)
                         .required(false)
                         .help("a line delimited list of hosts to scan")
-                        .value_parser(clap::value_parser!(std::path::PathBuf))
-            )
+                        .value_parser(clap::value_parser!(std::path::PathBuf)),
+                ),
         );
 
     let matches = cmd.get_matches();
@@ -36,10 +38,10 @@ fn main() {
 }
 
 pub const CLAP_STYLING: clap::builder::styling::Styles = clap::builder::styling::Styles::styled()
-.header(clap_cargo::style::HEADER)
-.usage(clap_cargo::style::USAGE)
-.literal(clap_cargo::style::LITERAL)
-.placeholder(clap_cargo::style::PLACEHOLDER)
-.error(clap_cargo::style::ERROR)
-.valid(clap_cargo::style::VALID)
-.invalid(clap_cargo::style::INVALID);
+    .header(clap_cargo::style::HEADER)
+    .usage(clap_cargo::style::USAGE)
+    .literal(clap_cargo::style::LITERAL)
+    .placeholder(clap_cargo::style::PLACEHOLDER)
+    .error(clap_cargo::style::ERROR)
+    .valid(clap_cargo::style::VALID)
+    .invalid(clap_cargo::style::INVALID);
